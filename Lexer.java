39f32e0f3a032f5d336b458bfc67385e280fee23
yuruj <hustyurj@163.com>
2021-04-02 23:02:30 +0800

    更新学习笔记

---
+++ Lexer.java
@@ -0,0 +1,22 @@
+package stone;
+
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Lexer {
+	public static String regexPat 
+		= "\\s*((//.*)|([0-9]+)|(\"(\\\\\"|\\\\\\\\|\\\\n|[^\"])*\")" 
+		  + "|[A-Z_a-z] [A-Z_a-z0-9]*|==|<=|>=|&&|\\|\\||\\p{Punct}?)";
+	private Pattern pattern = Pattern.compile(regexPat)
+	private ArrayList<Token> queue = new ArrayList<Token>();
+	private boolean hasMore;
+	private LineNumberReader reader;
+
+	public Lexer(Reader r) {
+		
+	}
+}
